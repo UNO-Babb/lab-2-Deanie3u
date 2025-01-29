ae6431bfcbf573b8b9a3b97626abd47c211f5a53
Deanie3u <geniet21@gmail.com>
2025-01-29 15:41:55 +0000

    Magic8ball done 1

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,20 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  answers = ["How are you?", "Hi!", "what's up?" "What are you doing today?", "Is that so?", "Oh I see", 
+             "It is wonderful!","What is your dream job?", "I am good!", "I am doing homework, wbu?",
+               "Do you feel like a leader or follower", " What's your dream job?", "My dream job is to be an Engineering."]
   #Answer question randomly with one of the options from your earlier list.
-
-
+  num = random.random() # decimal 0 - 1
+  num = num * 1000 #number 0 - 999 with decimals
+  num = int(num) #no more decimals
+  
+  things = len(answers)
+  num = num % things
+  question = input("Ask me a question: ")
+  print(answers[num])
+  
+  
+  
 if __name__ == '__main__':
   main()
